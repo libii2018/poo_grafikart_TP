82b2eec10261a6e72dddf82ee7bcbdd83a8a858f
Kratos <libiijoseph@gmail.com>
2022-06-29 12:05:41 +0100

    Singleton

--- app/Ap.php
+++ app/Ap.php
@@ -4,33 +4,42 @@ namespace App;
 
 class Ap {
 
-    const DB_NAME = 'Blog';
-    const DB_USER = 'root';
-    const DB_PASS = '';
-    const DB_HOST = 'localhost';
+    // const DB_NAME = 'Blog';
+    // const DB_USER = 'root';
+    // const DB_PASS = '';
+    // const DB_HOST = 'localhost';
 
-    private static $title = 'Mon super site';
+    private $title = 'Mon super site';
 
-    private static $database;
+    // private static $database;
 
-    public static function getDb() {
-        if(self::$database === null) {
-            self::$database = new Database(self::DB_NAME, self::DB_USER, self::DB_PASS, self::DB_HOST);
+    // public static function getDb() {
+    //     if(self::$database === null) {
+    //         self::$database = new Database(self::DB_NAME, self::DB_USER, self::DB_PASS, self::DB_HOST);
+    //     }
+    //     return self::$database;
+    // }
+
+    // public static function notFound() {
+    //     header("HTTP/1.0 404 Not Found");
+    //     header('Location:index.php?p=404');
+    // }
+
+    // public static function getTitle() {
+    //     return self::$title;
+    // }
+
+    // public static function setTitle($title) {
+    //     self::$title = $title . ' | ' . self::$title;
+    // }
+
+    private static $_instance;
+
+    public static function getInstance() {
+        if(is_null(self::$_instance)) {
+            self::$_instance = new Ap();
         }
-        return self::$database;
-    }
-
-    public static function notFound() {
-        header("HTTP/1.0 404 Not Found");
-        header('Location:index.php?p=404');
-    }
-
-    public static function getTitle() {
-        return self::$title;
-    }
-
-    public static function setTitle($title) {
-        self::$title = $title . ' | ' . self::$title;
+        return self::$_instance;
     }
 
 }
