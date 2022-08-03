9c1b480787f214b383c3fa5d7e78c2de3e812128
Kratos <libiijoseph@gmail.com>
2022-08-03 07:59:29 +0100

    Facade

--- app/Controller/DemoController.php
+++ app/Controller/DemoController.php
@@ -8,9 +8,16 @@ use Core\Database\QueryBuilder;
 class DemoController extends ApController {
 
     public function index() {
-        $query = new QueryBuilder();
-        echo $query
-            ->select('id','titre','contenu')
+        // Fluent
+        // $query = new QueryBuilder();
+        // echo $query
+        //     ->select('id','titre','contenu')
+        //     ->from('article','Post')
+        //     ->where('post.category_id = 1')
+        //     ->where('Post.date > NOW()');
+
+        require ROOT . '/Query.php';
+        echo \Query::select('id','titre','contenu')
             ->from('article','Post')
             ->where('post.category_id = 1')
             ->where('Post.date > NOW()');
